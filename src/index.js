3e6d7c707d00d10da85aeff113f0c36695931290
Johnathon Sykes <hireme@mrjsykes.com>
2017-11-17 16:06:25 -0800

    Lesson 11

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 //Create a new component. it should produce some html
-
 const App = () => {
   return <div>Hi!</div>
 }
 
 
-//The produced component should also get inserted into the DOM so we can see it in the browser.
-
-ReactDOM.render(App)
+//The produced component should also get inserted into the DOM so we can see it in the browser. .render() takes two arguments
+// the first argument: the <> wraps the const App into an instance.
+// second: Targets where the returned instance should be rendered
+ReactDOM.render(<App/>, document.querySelector('.container'))
